0698c5477dff5547f6197dd2b78d08b03636f8f0
bonedaddy <boned4ddy@protonmail.com>
2023-07-04 22:49:38 -0700

    wip testing of transaction broadcast abilities

--- api/api_test.go
+++ api/api_test.go
@@ -11,10 +11,12 @@ import (
 	"time"
 
 	"github.com/stretchr/testify/require"
+	"github.com/teamscanworks/breaker/breakerclient"
+	"github.com/teamscanworks/compass"
 	"go.uber.org/zap"
 )
 
-func TestAPI(t *testing.T) {
+func TestAPIDryRun(t *testing.T) {
 	ctx := context.Background()
 	logger, err := zap.NewDevelopment()
 	require.NoError(t, err)
@@ -28,6 +30,56 @@ func TestAPI(t *testing.T) {
 		DryRun:                       true,
 	})
 	require.NoError(t, err)
+	go func() {
+		api.Serve()
+	}()
+	api.logger.Info("issueing token")
+	jwtToken, err := api.jwt.Encode("apiTest", nil)
+	require.NoError(t, err)
+	api.logger.Info("issued token", zap.String("token", jwtToken))
+	api.logger.Info("executing webhook")
+	client := http.DefaultClient
+	payload := Payload{
+		Urls:    []string{"/cosmos/apiv1"},
+		Message: "amount > 1000",
+	}
+	data, err := json.Marshal(&payload)
+	require.NoError(t, err)
+	buffer := bytes.NewBuffer(data)
+	req, err := http.NewRequest("POST", "http://127.0.0.1:42690/v1/webhook", buffer)
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer: %s", jwtToken))
+	require.NoError(t, err)
+	res, err := client.Do(req)
+	require.NoError(t, err)
+	data, err = ioutil.ReadAll(res.Body)
+	require.NoError(t, err)
+	require.Equal(t, string(data), "dry run, skipping transaction invocation")
+
+	api.logger.Info("sleeping")
+	time.Sleep(time.Second * 5)
+	api.Close()
+}
+
+func TestAPI(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	logger, err := zap.NewDevelopment()
+	require.NoError(t, err)
+	cfg := compass.GetSimdConfig()
+	breaker, err := breakerclient.NewBreakerClient(ctx, logger, cfg)
+	require.NoError(t, err)
+	jwt := NewJWT("password123", "userId", 300)
+
+	api, err := NewAPI(ctx, logger, jwt, ApiOpts{
+		ListenAddress:                "127.0.0.1:42690",
+		Password:                     "password123",
+		IdentifierField:              "userId",
+		TokenValidityDurationSeconds: 300,
+		DryRun:                       false,
+	})
+	require.NoError(t, err)
+	api.WithBreakerClient(breaker)
+	t.Log("TODO: synchronize keyring with the simd environment")
 	go func() {
 		api.Serve()
 	}()
